f9c84be48d33cb24cc32710966e6a124830d4f13
Kranthiram <168562729+Kranthiram@users.noreply.github.com>
2025-01-22 13:04:19 +0530

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,38 @@
+//  DOM--document object model
+
+    //BOM--brower object model
+    //  window-->root element
+//window provides these APIs :- screen,history,fetch,navigator,navigation,storage,DOM
+
+// --------------------------------------------------------------------------------------------------------------------
+//     BOM-[browser object model]
+//      |  
+//   window--is root element for bom
+//      |
+//    _________________________________________________
+//    |    |       |       |        |           |     |    
+// screen history fetch navigator navigation storage dom
+
+
+// Dom-[document object model]
+//     root element-document[inside document html code will be their in tree structure]
+//          |
+//          html
+//          |
+//       ___________
+//       |         |
+//       head     body
+//         |        |
+//       _________   __
+//       |      |      |
+//       meta title     all tags 
+
+//dom is an object ,it is an api provided by window
+//it has root element called document.
+//it have life-span:until page reloads
+//it is used to create,update/modify,delete an element
+//we can provide style using DOM.
+//---------------------------------------------------------------------------------
+// How to covert HTML structure into DOM Tree:-
+                            
+
